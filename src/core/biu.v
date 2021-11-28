4e55ba6657287d5fc6ae8f74f0263ac762537844
0xtaruhi <zhang_zhengyi@outlook.com>
2021-11-28 13:19:30 +0800

    dele: jtag

---
+++ src/core/biu.v
@@ -0,0 +1,12 @@
+/*
+ * Description  : Bus Interface Unit
+ * Author       : Zhengyi Zhang
+ * Date         : 2021-11-25 17:10:47
+ * LastEditTime : 2021-11-28 13:16:30
+ * LastEditors  : Zhengyi Zhang
+ * FilePath     : \Buceros\src\core\biu.v
+ */
+module biu (
+);
+
+endmodule //biu
--- src/core/regfile.v
+++ src/core/regfile.v
@@ -2,7 +2,7 @@
  * Description  : Register
  * Author       : Zhengyi Zhang
  * Date         : 2021-11-25 16:40:59
- * LastEditTime : 2021-11-25 17:53:45
+ * LastEditTime : 2021-11-28 13:16:53
  * LastEditors  : Zhengyi Zhang
  * FilePath     : \Buceros\src\core\regfile.v
  */
@@ -11,14 +11,6 @@
 module regfile (
     input  wire               clk,
     input  wire               rst_n,
-
-    // jtag
-    `ifdef JTAG_ENABLE
-    input  wire [`RegAddrBus] jtag_addr_i,
-    input  wire               w_jtag_en_i,
-    input  wire [`RegDataBus] w_jtag_data_i,
-    output wire [`RegDataBus] r_jtag_data_o,
-    `endif
 
     input  wire [`RegAddrBus] r_addr1_i,
     input  wire [`RegAddrBus] r_addr2_i,
@@ -34,15 +26,11 @@ module regfile (
 
     assign r_data1_o = (r_addr1_i == w_addr_i) ? w_data_i : regfile[r_addr1_i];
     assign r_data2_o = (r_addr2_i == w_addr_i) ? w_data_i : regfile[r_addr2_i];
-    
-    `ifdef JTAG_ENABLE
-    assign r_jtag_data_o = regfile[jtag_addr_i];
-    `endif
-    
-    
+
     always @(*) begin
         regfile[0] = 0;             // x0 permanently be 0
     end
+
     genvar reg_idx;
     generate
         for(reg_idx=1;reg_idx<`REG_NUM;reg_idx=reg_idx+1) begin: loop_regfile
@@ -53,10 +41,6 @@ module regfile (
                 end else begin
                     if(w_en_i && w_addr_i == reg_idx) begin
                         regfile[reg_idx] <= w_data_i;
-                    `ifdef JTAG_ENABLE
-                    end else if(w_jtag_en_i && jtag_addr_i == reg_idx) begin
-                        regfile[reg_idx] <= w_jtag_data_i;
-                    `endif
                     end else begin
                         regfile[reg_idx] <= regfile[reg_idx];
                     end
