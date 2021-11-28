a5f03807feec93dfb05764a1d40e5dd8ea21f95a
wangbinlovecodes <1418851010@qq.com>
2021-11-28 23:45:32 +0800

    adder:ram,rom

--- src/perips/ram.v
+++ src/perips/ram.v
@@ -1,15 +1,36 @@
 `include "buceros_header.v"
 module ram (
-    input [`InstAddrBus] addr,
-    input we,
-    input re,
-    input sel,
-    input data_i,
-    input ce,
-    input rst,
-    input clk,
-    output data_o
-);
-
+    input                             clk,
+    input                             rst_n,
+    input [  `DataAddrBus]            addr_i,
+    input                             wmen_en_i,
+    input                             rmen_en_i,
+    input [      `DataBus]            data_i,
     
+    output[      `DataBus]            data_o
+);
+    reg [`RamBus] ram1 [`RamAddrBus];
+    reg [`RamBus] ram2 [`RamAddrBus];
+    reg [`RamBus] ram3 [`RamAddrBus];
+    reg [`RamBus] ram4 [`RamAddrBus];
+    // read
+    assign data_o = (rmen_en_i) ? {ram1[{addr_i[31:2],11}],ram2[{addr_i[31:2],10}],ram3[addr_i[31:2],01],ram4[addr_i[31:2],00]} : 32'hzzzz;
+    //write
+    always @(posedge clk) begin
+        if (ce) begin
+            else if (we) begin
+                ram1[addr_i[31:2],11] <= data_i[31:24];
+                ram2[addr_i[31:2],10] <= data_i[23:16];
+                ram3[addr_i[31:2],01] <= data_i[15:8];
+                ram4[addr_i[31:2],00] <= data_i[7:0];
+            end
+            else begin
+                ram1[addr_i[31:2],11] <= ram1[addr_i[31:2],11];
+                ram2[addr_i[31:2],10] <= ram2[addr_i[31:2],10];
+                ram3[addr_i[31:2],01] <= ram3[addr_i[31:2],01];
+                ram4[addr_i[31:2],00] <= ram4[addr_i[31:2],00];
+            end
+        end
+    end
+
 endmodule
--- src/perips/rom.v
+++ src/perips/rom.v
@@ -1,10 +1,37 @@
-`include "define.v"
+
+`include "buceros_header.v"
 module rom (
-    input addr_i,
-    input ce,
-    output addr_o,
-    output inst
+    input                             clk,
+    input                             rst_n,
+    input [  `InstAddrBus]            InstBus_i,
+    input                             winst_en_i,
+    input                             rinst_en_i,
+    input [      `DataBus]            inst_i,
+    
+    output[      `DataBus]            inst_o
 );
-    
-    
+    reg [`RomBus] rom1 [`RomAddrBus];
+    reg [`RomBus] rom2 [`RomAddrBus];
+    reg [`RomBus] rom3 [`RomAddrBus];
+    reg [`RomBus] rom4 [`RomAddrBus];
+    // read
+    assign data_o = (rmen_en_i) ? {rom1[{inst_i[31:2],11}],rom2[{inst_i[31:2],10}],rom3[inst_i[31:2],01],rom4[inst_i[31:2],00]} : 32'hzzzz;
+    //write
+    always @(posedge clk) begin
+        if (ce) begin
+            else if (we) begin
+                rom1[inst_i[31:2],11] <= data_i[31:24];
+                rom2[inst_i[31:2],10] <= data_i[23:16];
+                rom3[inst_i[31:2],01] <= data_i[15:8];
+                rom4[inst_i[31:2],00] <= data_i[7:0];
+            end
+            else begin
+                rom1[inst_i[31:2],11] <= rom1[inst_i[31:2],11];
+                rom2[inst_i[31:2],10] <= rom2[inst_i[31:2],10];
+                rom3[inst_i[31:2],01] <= rom3[inst_i[31:2],01];
+                rom4[inst_i[31:2],00] <= rom4[inst_i[31:2],00];
+            end
+        end
+    end
+
 endmodule
