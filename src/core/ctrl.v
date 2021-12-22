07373ce63a23b50b7ce380aafbd48004001c8a9e
ShaoqunLi <shaoqunli@163.com>
2021-12-22 11:25:09 +0800

    fix: ctrl

--- src/core/ctrl.v
+++ src/core/ctrl.v
@@ -12,12 +12,16 @@ module ctrl (
 
     output wire [4:0] stall_o
 );
-    // version 2.0 including stallreq from id_ex ONLY
-    assign stall_o[0] = stallreq_id_ex_i;   // to pc_reg
-    assign stall_o[1] = stallreq_id_ex_i;   // to if_id
-    assign stall_o[2] = stallreq_id_ex_i;   // to id_ex
-    assign stall_o[3] = 1'b0;               // to ex_mem
-    assign stall_o[4] = stallreq_id_ex_i;   // to mem_wb
+
+    wire enter_n;
+
+    assign enter_n = ~enter_i;
+    // version 2.2 including stallreq from id_ex ONLY
+    assign stall_o[0] = rst_n & enter_n & stallreq_id_ex_i; // to pc_reg
+    assign stall_o[1] = rst_n & enter_n & stallreq_id_ex_i; // to if_id
+    assign stall_o[2] = rst_n & enter_n & stallreq_id_ex_i; // to id_ex
+    assign stall_o[3] = rst_n & enter_n & 1'b0;             // to ex_mem
+    assign stall_o[4] = rst_n & enter_n & 1'b0;             // to mem_wb
     // assign stall_o[5] = stallreq_id_ex_i;   // to regfile
 
     // version 1.0
