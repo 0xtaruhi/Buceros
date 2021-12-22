ea6a038418101da07922d9e587c76c637bd51783
0xtaruhi <zhang_zhengyi@outlook.com>
2021-12-22 13:24:23 +0800

    add: test C file

--- tests/temp2/temp2.c
+++ tests/temp2/temp2.c
@@ -1,27 +1,52 @@
-/*
- * Description  : 
- * Author       : Zhengyi Zhang
- * Date         : 2021-12-22 00:38:56
- * LastEditTime : 2021-12-22 01:09:12
- * LastEditors  : Zhengyi Zhang
- * FilePath     : \Buceros\tests\temp2\temp2.c
- */
-
 #include <stdint.h>
+#include "../include/gpio.h"
+
+void delay(uint32_t);
 
 int main()
 {
-    int i, j, sum;
-    int* ptr = (int*)0x20000000;
-    for(i=1; i<1000; i++){
-        sum = 0;
-        for(j = 1; j < i; j++){
-            if( i % j == 0 ){
-                sum += j;
-            }
+    uint32_t seg_disp[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
+    uint32_t disp_num[4] = {0};
+    uint32_t disp_sel = 1;
+    uint32_t i, j;
+    while(1)
+    {
+        for(i = 0; i < 50; ++i){
+            GPIO_REG((GPIO_SEG_SEL)) = 1;
+            GPIO_REG(GPIO_SEG) = seg_disp[disp_num[0]];
+            delay(500000);
+            GPIO_REG((GPIO_SEG_SEL)) = 2;
+            GPIO_REG(GPIO_SEG) = seg_disp[disp_num[1]];
+            delay(500000);
+            GPIO_REG((GPIO_SEG_SEL)) = 4;
+            GPIO_REG(GPIO_SEG) = seg_disp[disp_num[2]];
+            delay(500000);
+            GPIO_REG((GPIO_SEG_SEL)) = 8;
+            GPIO_REG(GPIO_SEG) = seg_disp[disp_num[3]];
+            delay(500000);
         }
-        if(sum == i){
-            *(ptr++) = i;
+        disp_num[0] += 1;
+        if(disp_num[0] == 10){
+            disp_num[0] = 0;
+            disp_num[1] +=1;
+        }
+        if(disp_num[1] == 10){
+            disp_num[1] = 0;
+            disp_num[2] += 1;
+        }
+        if(disp_num[2] == 10){
+            disp_num[2] = 0;
+            disp_num[3] += 1;
+        }
+        if(disp_num[3] == 10){
+            disp_num[3] = 0;
         }
     }
+}
+
+void delay(uint32_t num)
+{
+    int i = 0;
+    for(i = 0; i < num; ++i);
+    return;
 }
